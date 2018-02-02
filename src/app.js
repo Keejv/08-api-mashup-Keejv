f2b929120c473495948eaf0e832dab14e3362044
Keejv <kaveh.haddad@chasacademy.se>
2018-02-02 17:31:44 +0100

    add search fucntion with flicker API response. Add result view in DOM

--- src/app.js
+++ src/app.js
@@ -5,25 +5,45 @@ class Mashed {
   constructor(element) {
     this.root = element;
 
-    this.fetchFlickrPhotos();
-    this.fetchWordlabWords('detest');
+    var searchBtn = document.getElementById('searchBtn')                // skapa en variabel koppla på id searchBtn
+    searchBtn.addEventListener('click', () => {                         // lyssna efter click
+     var searchRequest = document.getElementById('searchInput').value;  //spara input i searchRequest
+     
+     this.fetchFlickrPhotos(searchRequest);
+     this.fetchWordlabWords('detest');
+
+     console.log(searchRequest);
+    })
+
   }
 
-  fetchFlickrPhotos() {
+  flickerResponse(res){                                                 // Funktionen flickerResponse(parameter=res) mappar och renderar ut bilden
+    let flickerPicture = '';                                            // Skapar en tom variabel som ska fyllas med innehåll(flickerPicture)
+    res.photos.photo.map((photo) => {                                   // Här mappar vi till url_m
+      flickerPicture += 
+      `<li class="list-group-items" id="results">
+          <p><img src="${photo.url_m}"></p>
+       </li>`
+    })
+    document.querySelector('.list-group').innerHTML = flickerPicture;   //här sparar vi resultat som vi sedan skickar ut i DOMen
+  }
+
+  fetchFlickrPhotos(searchRequest) {                                  // Denna fetch kollar på variabeln searchRequest
     let resourceUrl =
       'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='
     let flickrAPIkey = process.env.myKittyKey
 
     let flickrQueryParams =
-      '&text=space' +
-      '&extras=url_q&format=json&nojsoncallback=1'
+      '&text=' + searchRequest +                                        //Vad variabeln flickrQueryParams ska söka efter
+      '&extras=url_m&format=json&nojsoncallback=1'
     let flickrUrl = resourceUrl + flickrAPIkey + flickrQueryParams
 
     fetch(flickrUrl)
       .then(res => res.json())
       .then(res => {
-        console.log('Got response from FlickR!')
-        console.log(res)
+        // console.log('Got response from FlickR!')
+        // console.log(res.photos.photo[0].url_q)                            // Using the console.log to traversing in i "the rabbithole"
+      this.flickerResponse(res)                                              // fetchFlickerPhotots skickar response till funktioen flickerResponse
       })
       .catch(err => console.error(err))
   }
